84dc70d9d2b6fdbdd1e6591d210da90c07e4cdab
GrayJunZi <GrayJunZi@outlook.com>
2023-12-27 08:28:50 +0800

    4.13 添加 starred_repos_repository

--- repo_viewer/lib/github/repos/starred_repos/infrastructure/starred_repos_remote_service.dart
+++ repo_viewer/lib/github/repos/starred_repos/infrastructure/starred_repos_remote_service.dart
@@ -1,5 +1,8 @@
 import 'package:dio/dio.dart';
+import 'package:repo_viewer/core/infrastructure/dio_extensions.dart';
+import 'package:repo_viewer/core/infrastructure/network_exceptions.dart';
 import 'package:repo_viewer/core/infrastructure/remote_response.dart';
+import 'package:repo_viewer/github/repos/core/infrastructure/github_headers.dart';
 import 'package:repo_viewer/github/repos/core/infrastructure/github_headers_cache.dart';
 import 'package:repo_viewer/github/repos/core/infrastructure/github_repo_dto.dart';
 
@@ -12,6 +15,58 @@ class StarredReposRemoteService {
   Future<RemoteResponse<List<GithubRepoDTO>>> getStarredReposPage(
     int page,
   ) async {
-    _dio.get('');
+    const token = 'ghp_rtAXRef8yKlwNr17a5N7pYjJSaOcLW91mh5Kw';
+    const accept = 'application/vnd.github.v3.param+json';
+    final requestUri = Uri.https(
+      'api.github.com',
+      '/user/starred',
+      {'page': '$page'},
+    );
+
+    final previousHeaders = await _headersCache.getHeaders(requestUri);
+
+    try {
+      final response = await _dio.getUri(
+        requestUri,
+        options: Options(
+          headers: {
+            'Authorization': 'bearer $token',
+            'Accept': accept,
+            'If-None-Match': previousHeaders?.etag ?? '',
+          },
+        ),
+      );
+
+      if (response.statusCode == 304) {
+        return RemoteResponse.notModified(
+          maxPage: previousHeaders?.link?.maxPage ?? 0,
+        );
+      }
+
+      if (response.statusCode == 200) {
+        final headers = GithubHeaders.parse(response);
+
+        await _headersCache.saveHeaders(requestUri, headers);
+        final convertedData = (response.data as List<dynamic>)
+            .map((e) => GithubRepoDTO.fromJson(e as Map<String, dynamic>))
+            .toList();
+        return RemoteResponse.withNewData(
+          convertedData,
+          maxPage: headers.link?.maxPage ?? 1,
+        );
+      }
+
+      throw RestApiException(response.statusCode);
+    } on DioException catch (e) {
+      if (e.isNoConnectionError) {
+        return RemoteResponse.noConnection(
+          maxPage: previousHeaders?.link?.maxPage ?? 0,
+        );
+      } else if (e.response != null) {
+        throw RestApiException(e.response?.statusCode);
+      } else {
+        rethrow;
+      }
+    }
   }
 }
